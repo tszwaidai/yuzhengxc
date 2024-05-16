b4e18b234adfe05edf742489a2f04ba2d2a12ce8
tszwaidai <tszwaidai@163.com>
2024-05-16 20:18:59 +0800

    登录bg

---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="login">
+        <div class="login-box">
+            <span>宁波市渔政 </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    background-image: url('../assets/bg_bottom.png');
+}
+</style>
